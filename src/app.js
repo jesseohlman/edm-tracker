aee8bec29aa4afff54b8aea5a137dc410f1b8908
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-05-11 11:21:30 -0400

    home page created

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config");
+const mainRoute = require("./routes/static");
 
 appConfig.init(app, express);
 //routeConfig.init(app);
-
+app.use("/", mainRoute);
 
 module.exports = app;
---
+++ src/routes/static.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const staticController = require("../controllers/staticController");
+
+router.get("/", staticController.index);
+
+module.exports = router;
